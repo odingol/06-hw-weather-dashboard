d0be81477414e3ee2d09caa0d68efb952ebce672
Lamor Odingo <l2odingo@gmail.com>
2022-02-12 02:42:18 -0800

    added forecast

--- develop/script.js
+++ develop/script.js
@@ -4,7 +4,7 @@ var cityName = 'Seattle'; //After the code completes get rid of this VARIABLE as
 
 
 function weatherData() { //function weatherData should have "cityName" inside to later be changed using the search button listener (which is var cityInput)//
-    var requestURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${APIKey}`;
+    var requestURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${APIKey}`
 
     fetch(requestURL)
     .then(function(response) {
@@ -13,28 +13,80 @@ function weatherData() { //function weatherData should have "cityName" inside to
     
     .then(function(data) {
         console.log(data);
+        displayForecast(data.coord.lat, data.coord.lon);
         let weatherTitle = $('.title');
         let icon = data.weather[0].icon;
-        weatherIcon = $(`<img src="http://openweathermap.org/img/w/${icon}.png"/>`);
+        let weatherIcon = $(`<img src="http://openweathermap.org/img/w/${icon}.png"/>`);
         let dateToday = new Date(data.dt * 1000).toLocaleDateString("en-US")
         $('.title').text(`${data.name} (${dateToday}) `);
         $('.temp').text(`Temp: ${data.main.temp} ℉`);
         $('.wind').text(`Wind: ${data.wind.speed} MPH`);
         $('.humidity').text(`Humidity: ${data.main.humidity} %`);
         $('.uv-index').text(`UV Index:`);
-        
-        weatherTitle.append(weatherIcon);
-        
 
+        weatherTitle.append(weatherIcon);
     })
 
+  
 }
 
 weatherData();
 
 
+function displayForecast(lat, lon) {
+    var geoLocate = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${APIKey}`
+    fetch(geoLocate)
+    .then(function(response) {
+        return response.json();
+    })
+    
+    .then(function(data) {
+        console.log(data);
+        
+        
+        for(var i = 1; i < 6; i++) {
 
-// function retrieveHistory(lat, lon) {
+        let forecastDate = $(`.date${i}`);
+        let forecastTemp = $(`.temp-text${i}`);
+        let forecastWind = $(`.wind-text${i}`);
+        let forecastHumidity = $(`.humidity-text${i}`);
+        
+        
+        let dates = new Date(data.daily[i].dt *1000).toLocaleDateString("en-US")
+        let weatherIcon = data.daily[i].weather[0].icon;
+        let forecastIcon = $(`<img src=http://openweathermap.org/img/w/${weatherIcon}.png class=card-img-top icon${i}>`);
+
+        forecastDate.text(dates);
+        forecastTemp.text(`Temp: ${data.daily[i].temp.day} ℉`);
+        forecastWind.text(`Wind: ${data.daily[i].wind_speed} MPH`);
+        forecastHumidity.text(`Humidity: ${data.daily[i].humidity} %`);
+        // forecastIcon.setAttribute("src", "http://openweathermap.org/img/w/" + weatherIcon + ".png");
+
+        forecastDate.append(forecastIcon);
+
+
+
+        
+        }
+        
+        
+        
+        
+        
+    });
+
+
+
+}
+ 
+
+
+
+
+
+
+
+// function retrieveHistory(cityName) {
 //     var 
 
 
